884ff45f5789756272d19f676daafce5e0bd2c09
Treyceratops <treytowers@yahoo.com>
2023-02-02 18:53:45 -0800

    add typewriter effect

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import homePic from '../images/home-pic.JPG';
+import TypeWriterEffect from 'react-typewriter-effect';
 
 function Home(props) {
 	return (
 		<div className='home'>
-			<h1 className='intro-text'>Live the Way You Want to Live...</h1>
+			{/* <h1 className='intro-text'>Live the Way You Want to Live...</h1> */}
+			<TypeWriterEffect
+				textStyle={{
+					color: 'white',
+					// fontSize: '2em',
+					fontFamily: 'Sacramento',
+				}}
+				// startDelay={0}
+				cursorColor='white'
+				multiText={[
+					'Towers Property Renovation',
+					'Live the Way You Want to Live...',
+				]}
+				multiTextDelay={2000}
+				typeSpeed={60}
+			/>
 		</div>
 	);
 }
